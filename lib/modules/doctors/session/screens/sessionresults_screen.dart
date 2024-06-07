79bd11e7179de5387ac2b7aac5a47a0c382b22a0
Somnathumapathi <somnathumapathi7@gmail.com>
2024-06-08 01:05:49 +0530

    something

--- lib/modules/doctors/session/screens/sessionresults_screen.dart
+++ lib/modules/doctors/session/screens/sessionresults_screen.dart
@@ -13,6 +13,7 @@ class _SessionResultScreenState extends State<SessionResultScreen> {
   late TextEditingController _resultController;
   final _durationController = TextEditingController();
   final _patientNameController = TextEditingController();
+  final _patientNumberController = TextEditingController();
   String? result;
 
   void _generateResultText() {
@@ -50,6 +51,7 @@ $medications
     _resultController.dispose();
     _durationController.dispose();
     _patientNameController.dispose();
+    _patientNumberController.dispose();
     super.dispose();
   }
 
@@ -64,18 +66,68 @@ $medications
         child: Center(
             child: Column(
           children: [
-            TextFormField(
-              controller: _resultController,
-              maxLines: 20,
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 91, 106, 191)),
+              child: TextFormField(
+                decoration:
+                    InputDecoration(fillColor: Colors.indigo.withOpacity(0.26)),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300),
+                controller: _resultController,
+                maxLines: 20,
+              ),
             ),
             SizedBox(
               height: 15,
             ),
-            Text("Enter patient name:"),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Enter Patient Name"),
-              controller: _patientNameController,
-              //keyboardType: TextInputType.numberWithOptions(),
+            Text(
+              "Enter patient name:",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 91, 106, 191)),
+              child: TextFormField(
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300),
+                decoration: InputDecoration(hintText: "Enter Patient Name"),
+                controller: _patientNameController,
+                //keyboardType: TextInputType.numberWithOptions(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Enter patient number:",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 91, 106, 191)),
+              child: TextFormField(
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300),
+                decoration: InputDecoration(hintText: "Enter Patient Number"),
+                controller: _patientNumberController,
+                //keyboardType: TextInputType.numberWithOptions(),
+              ),
             ),
             ElevatedButton.icon(
               onPressed: () {},
